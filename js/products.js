a7f8908af563949a298d305e33ea7c4f4223b159
hammadtariq838 <hammadtariq838@gmail.com>
2024-09-18 23:27:25 +0500

    fix: products path to correctly display assets

--- js/products.js
+++ js/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
-    imageUrl: '/images/airpods.jpg',
+    imageUrl: './images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     price: 89.99,
@@ -10,7 +10,7 @@ const products = [
   {
     id: '2',
     name: 'iPhone 13 Pro 256GB Memory',
-    imageUrl: '/images/phone.jpg',
+    imageUrl: './images/phone.jpg',
     description:
       'Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     price: 599.99,
@@ -18,7 +18,7 @@ const products = [
   {
     id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
-    imageUrl: '/images/camera.jpg',
+    imageUrl: './images/camera.jpg',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     price: 929.99,
@@ -26,7 +26,7 @@ const products = [
   {
     id: '4',
     name: 'Sony Playstation 5',
-    imageUrl: '/images/playstation.jpg',
+    imageUrl: './images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     price: 399.99,
@@ -34,7 +34,7 @@ const products = [
   {
     id: '5',
     name: 'Logitech G-Series Gaming Mouse',
-    imageUrl: '/images/mouse.jpg',
+    imageUrl: './images/mouse.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     price: 49.99,
@@ -42,7 +42,7 @@ const products = [
   {
     id: '6',
     name: 'Amazon Echo Dot 3rd Generation',
-    imageUrl: '/images/alexa.jpg',
+    imageUrl: './images/alexa.jpg',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     price: 29.99,
